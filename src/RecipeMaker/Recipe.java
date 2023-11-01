c29a2be385bda131810c08dce81990b0f2078e34
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2023-11-01 13:39:45 -0700

    Added a Recipe class

---
+++ src/RecipeMaker/Recipe.java
@@ -0,0 +1,24 @@
+package RecipeMaker;
+
+public class Recipe {
+    private String title;
+    private String description;
+
+    Recipe(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String newDesc){
+        description = newDesc;
+    }
+
+}
