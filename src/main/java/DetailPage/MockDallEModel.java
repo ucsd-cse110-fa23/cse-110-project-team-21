6841fd77c1cf2c534bad05cbee56196e79e5b069
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2023-12-04 18:31:19 -0800

    US3 c) updated mock tests to reflect DallEModel fixes

--- src/main/java/DetailPage/MockDallEModel.java
+++ src/main/java/DetailPage/MockDallEModel.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import Main.Main;
 /**
  * I think I found a problem. If we use the query to be the URL version of the recipe title, how are we
  * finding an existing image name if they all start with "="?
@@ -17,16 +18,20 @@ import java.net.URL;
 public class MockDallEModel {
 
     public String performImageRequest(String query, ArrayList<String> mockDirectory) {
-        //File tmpDir = new File(query + ".jpg");
-        //String tmpDir = query + ".jpg";
-        boolean exists = mockDirectory.contains("=" + query + ".jpg");
-        //boolean exists = (mockDirectory.size() > 0);
 
+        query = query.replace(" ", "%20");
+        /*
+        System.out.println("Query: " + query);
+        File tmpDir = new File("=" + query + ".jpg");
+        boolean exists = tmpDir.exists();
+        */
+        boolean exists = mockDirectory.contains("=" + query + ".jpg");
         if(exists) {
-            return "=" + query + ".jpg [already existed]";
+            return "="+ query + ".jpg [already existed]";
         }
         try {
-            String urlString = "http://localhost:8100/dalle/";
+            //String urlString = "http://localhost:8100/dalle/";
+            String urlString = Main.HOSTNAME_URL + "/dalle";
             if (query != null) {
                 urlString += "?=" + query.replace(":", "_");
             }
@@ -57,7 +62,7 @@ public class MockDallEModel {
         }
         public void setRequestMethod(String method){
             if(method.equals("GET")){
-                String s = url.toString().replace("http://localhost:8100/dalle/?", "");
+                String s = url.toString().replace(Main.HOSTNAME_URL + "/dalle" + "?", "");
                 inputStream = s;
             }
         }
--- src/test/java/DallEJunitTest.java
+++ src/test/java/DallEJunitTest.java
@@ -45,9 +45,13 @@ public class DallEJunitTest {
         mockDirectory.add("=Coconut%20Fish%20Lunch.jpg");
         mockDirectory.add("=Tomato,%20Onion,%20and%20Bread%20Casserole.jpg");
         
-        RecipeModel recipe = new RecipeModel("Tomato, Onion, and Bread Casserole", ""); 
-        String previewImgFileName = dallE.performImageRequest(recipe.getTitle().replace(" ", "%20"), mockDirectory);
-        assertEquals(previewImgFileName,"=Tomato,%20Onion,%20and%20Bread%20Casserole.jpg [already existed]");
+        RecipeModel recipe0 = new RecipeModel("Bacon, Syrup, and Pancake Stack", ""); 
+        String previewImgFileName0 = dallE.performImageRequest(recipe0.getTitle().replace(" ", "%20"), mockDirectory);
+        assertEquals(previewImgFileName0,"=Bacon,%20Syrup,%20and%20Pancake%20Stack.jpg [already existed]");
+        
+        RecipeModel recipe1 = new RecipeModel("Tomato, Onion, and Bread Casserole", ""); 
+        String previewImgFileName1 = dallE.performImageRequest(recipe1.getTitle().replace(" ", "%20"), mockDirectory);
+        assertEquals(previewImgFileName1,"=Tomato,%20Onion,%20and%20Bread%20Casserole.jpg [already existed]");
         
         RecipeModel recipe2 = new RecipeModel("Tomato, Onion, and Bread Foie gras", "");
         String previewImgFileName2 = dallE.performImageRequest(recipe2.getTitle().replace(" ", "%20"), mockDirectory);
