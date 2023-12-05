3f9a08801cb65ad9784b5bb0a8dbc3e3c2121504
ShangLiu001 <108479761+ShangLiu001@users.noreply.github.com>
2023-12-04 17:05:40 -0800

    Update HomeController.java
    debug

--- src/main/java/HomePage/HomeController.java
+++ src/main/java/HomePage/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
 
     HomeView mainPageView;
     HomeModel mainPageModel = new HomeModel(this);
+    List<RecipeModel> fullList = new ArrayList<>();
 
     public HomeController(HomeView mainPageView) {
         this.mainPageView = mainPageView;
@@ -38,6 +39,7 @@ public class HomeController {
         this.mainPageView.recipeList.getChildren().clear();
         try{
             List<RecipeModel> newRecipeList = mainPageModel.loadFromDatabase();
+            this.fullList = newRecipeList;
             for(RecipeModel r: newRecipeList){
                 MainPageRecipeCard toAdd = new MainPageRecipeCard(r.getTitle(), r.getMealType());
                 this.mainPageView.recipeList.getChildren().add(toAdd);
@@ -67,67 +69,31 @@ public class HomeController {
         });
 
         this.mainPageView.getHeader().getSortDropDown().setOnAction(e -> {
-            ComboBox<String> sortDropDown = this.mainPageView.getHeader().getSortDropDown();
-            String selectedOption = sortDropDown.getValue();
-            System.out.println("Selected Option: " + selectedOption);
-
-            List<RecipeModel> sortedList = loadCurrentList();
-            List<RecipeModel> dateList = loadCurrentList();
-
-            for (int i = 0; i < sortedList.size(); i++) {
-                System.out.println(sortedList.get(i).getTitle());
+            System.out.println("Step 4");
+            List<RecipeModel> curList = this.fullList;
+            String needSort = this.mainPageView.getHeader().getSortDropDown().getValue();
+            String needFilter = this.mainPageView.getHeader().getFilterDropDown().getValue(); 
+            if (needSort != "Sort" ) {
+                curList = sortList(curList);                
             }
-
-            for (int i = 0; i < dateList.size(); i++) {
-                System.out.println(dateList.get(i).getTitle());
+            if (needFilter != "Filter") {
+                curList = filterList(curList);
             }
-
-            Comparator<RecipeModel> titleComparator = Comparator.comparing(RecipeModel::getTitle);
-            Comparator<RecipeModel> dateComparator = Comparator.comparing(RecipeModel::getIndex);
-
-            dateList = sortedList.stream().collect(Collectors.toList());
-
-            Collections.sort(sortedList, titleComparator);
-            Collections.sort(dateList, dateComparator);
-
-            switch (selectedOption) {
-                case "A-Z":
-                    showSortedRecipe(sortedList);
-                    break;
-
-                case "Z-A":
-                    Collections.reverse(sortedList);
-                    showSortedRecipe(sortedList);
-                    break;
-                
-                case "Old-New":
-                    showSortedRecipe(dateList);
-                    break;
-                
-                case "New-Old":
-                    Collections.reverse(dateList);
-                    showSortedRecipe(dateList);
-                    break;
-            }
+            showSortedRecipe(curList);
         });
 
         this.mainPageView.getHeader().getFilterDropDown().setOnAction(e -> {
-            ComboBox<String> filterDropDown = this.mainPageView.getHeader().getFilterDropDown();
-            String selectedOption = filterDropDown.getValue();
-            System.out.println("Selected Option: " + selectedOption);
-            List<RecipeModel> filteredList = mainPageModel.loadFromDatabase();
-            mainPageView.getHeader().getSortDropDown().setValue("Sort");
-            if (selectedOption.equals("AllFilter")) {
-                showSortedRecipe(filteredList);
-                return;
-            };
-
-            filteredList = filteredList.stream()
-                .filter(recipe -> selectedOption.equals(recipe.getMealType()))
-                .collect(Collectors.toList());
-
-
-            showSortedRecipe(filteredList);
+            System.out.println("Step 4");
+            List<RecipeModel> curList = this.fullList;
+            String needSort = this.mainPageView.getHeader().getSortDropDown().getValue();
+            String needFilter = this.mainPageView.getHeader().getFilterDropDown().getValue(); 
+            if (needSort != "Sort" ) {
+                curList = sortList(curList);                
+            }
+            if (needFilter != "Filter") {
+                curList = filterList(curList);
+            }
+            showSortedRecipe(curList);
         });
 
     }
@@ -159,5 +125,88 @@ public class HomeController {
 
         }
         return newRecipeList;
+    }
+
+    public List<RecipeModel> sortList(List<RecipeModel> completeList){
+        ComboBox<String> sortDropDown = this.mainPageView.getHeader().getSortDropDown();
+        String selectedOption = sortDropDown.getValue();
+        System.out.println("Selected Option: " + selectedOption);
+
+        List<RecipeModel> sortedList = completeList;
+        List<RecipeModel> dateList = completeList;
+
+        Comparator<RecipeModel> titleComparator = Comparator.comparing(RecipeModel::getTitle);
+        Comparator<RecipeModel> dateComparator = Comparator.comparing(RecipeModel::getIndex);
+
+        dateList = sortedList.stream().collect(Collectors.toList());
+
+        Collections.sort(sortedList, titleComparator);
+        Collections.sort(dateList, dateComparator);
+
+        switch (selectedOption) {
+            case "A-Z":
+                return sortedList;
+
+            case "Z-A":
+                Collections.reverse(sortedList);
+                return sortedList;       
+            
+            case "Old-New":
+                return dateList;       
+            
+            case "New-Old":
+                Collections.reverse(dateList);
+                return dateList;
+        }
+
+        return sortedList;
+    }
+
+    public List<RecipeModel> filterList (List<RecipeModel> completeList){
+        ComboBox<String> filterDropDown = this.mainPageView.getHeader().getFilterDropDown();
+        String selectedOption = filterDropDown.getValue();
+        System.out.println("Selected Option: " + selectedOption);
+        List<RecipeModel> filteredList = completeList;
+        // mainPageView.getHeader().getSortDropDown().setValue("Sort");
+        if (selectedOption.equals("AllFilter")) {
+            return completeList;
+        };
+
+        filteredList = filteredList.stream()
+            .filter(recipe -> selectedOption.equals(recipe.getMealType()))
+            .collect(Collectors.toList());
+
+        return filteredList;
+    }
+
+    public List<RecipeModel> mockSortedList(List<RecipeModel> sortedList, String selectedOption){
+        Comparator<RecipeModel> titleComparator = Comparator.comparing(RecipeModel::getTitle);
+        Comparator<RecipeModel> dateComparator = Comparator.comparing(RecipeModel::getIndex);
+
+        List<RecipeModel> dateList = sortedList;
+
+        Collections.sort(sortedList, titleComparator);
+        Collections.sort(dateList, dateComparator);
+
+         switch (selectedOption) {
+            case "A-Z":
+                showSortedRecipe(sortedList);
+                return sortedList;
+
+            case "Z-A":
+                Collections.reverse(sortedList);
+                showSortedRecipe(sortedList);
+                return sortedList; 
+            
+            case "Old-New":
+                showSortedRecipe(dateList);
+                return sortedList;
+            
+            case "New-Old":
+                Collections.reverse(dateList);
+                showSortedRecipe(dateList);
+                return sortedList;
+        }
+        return sortedList;
     }
 }
