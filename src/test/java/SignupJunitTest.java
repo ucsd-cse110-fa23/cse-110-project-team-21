7e55283934cba6def53c49a19ecfe205c9a5cd3b
ShangLiu001 <shl066@ucsd.edu>
2023-12-05 22:47:39 -0800

    update login&sign up mock, bug fixing

--- src/test/java/SignupJunitTest.java
+++ src/test/java/SignupJunitTest.java
@@ -17,35 +17,35 @@ public class SignupJunitTest {
     @Test
     void testSignup() throws Exception {
         SignUpController controller = new SignUpController(null);
-        controller.signUpHelper(null, "test", "test", "test", true);
+        controller.signUpHelper(null, "testusername", "testpassword", "testpassword", true);
 
         // read from the mockUsers.txt file
         String pathName = "mockUsers.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
-            reader.readLine();
-            assertEquals(reader, "Username: test");
-            reader.readLine();
-            assertEquals(reader, "Password: test");
+            String temp = reader.readLine();
+            assertEquals(temp, "testusername");
+            temp = reader.readLine();
+            assertEquals(temp, "testpassword");
             reader.close();
         } catch (Exception e) {
             System.out.println("Could not initialize FileReader with specified input file");
         }
     }
 
-
     @Test
     void testSignup2() throws Exception {
         SignUpController controller = new SignUpController(null);
-        controller.signUpHelper(null, "test", "test", "test", true);
+        controller.signUpHelper(null, "testusername2", "testpassword2", "testpassword2", true);
 
         // read from the mockUsers.txt file
         String pathName = "mockUsers.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
-            assertEquals(reader, "Username: test");
-            reader.readLine();
-            assertEquals(reader, "Password: test");
-            reader.readLine();
-            assertEquals(reader, "");
+            String temp = reader.readLine();
+            assertEquals(temp, "testusername2");
+            temp = reader.readLine();
+            assertEquals(temp, "testpassword2");
+            temp = reader.readLine();
+            assertEquals(temp, null);
             reader.close();
         } catch (Exception e) {
             System.out.println("Could not initialize FileReader with specified input file");
